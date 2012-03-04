fb310dd95f63e0b56acccf9207c596ca2accfbc3
per <per@karlssonper.com>
2012-03-03 16:36:32 -0800

    Node class

--- src/Node.h
+++ src/Node.h
@@ -10,6 +10,31 @@
 
 class Node
 {
+public:
+    Node(std::string _name, Node * _parent = NULL);
+    const std::string & name() const { return name_;};
+    const Matrix4 & modelMtx() const { return modelTransform_; };
+    void parentIs(Node * _parent);
+    void rotateX(float _degrees);
+    void rotateY(float _degrees);
+    void rotateZ(float _degrees);
+    void rotate(float _degrees, const Vector3 &_axis);
+    void translate(Vector3 _T);
+    void scale(Vector3 _S);
+    void update();
+private:
+    Node();
+    Node(const Node &);
+    void operator=(const Node &);
 
+    std::string name_;
+    Matrix4 modelTransformLocal_;
+    Matrix4 modelTransformGlobal_;
+    Node * parent_;
+    std::list<Node*> children_;
+    bool modelTransformLocalChanged_;
+    void addChildren(Node* _children);
+    void removeChildern(Node * _children);
+    void update(bool _needsUpdate);
 };
 #endif /* NODE_H_ */
