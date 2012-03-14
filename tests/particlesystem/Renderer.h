cb3bbfa420f6c56ad594a8411e00cd656de1d848
Victor Sand <victor.sand@gmail.com>
2012-03-14 13:24:25 -0700

    moving tests around

---
+++ tests/particlesystem/Renderer.h
@@ -0,0 +1,22 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include "../ParticleSystem.h"
+#include "Shader.h"
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+#include <GL/FreeImage.h>
+
+class Renderer {
+public:
+    Renderer(ParticleSystem *_ps, Shader *shader);
+    void render();
+    GLuint loadTexture(std::string _source);
+private:
+    ParticleSystem *ps_;
+    Shader *shader_;
+};
+
+#endif
